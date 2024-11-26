750dce417fd6c1ebe8d355da21b1c7eda2fa0696
Shannon Tetley <tetley_73@hotmail.com>
2024-11-26 15:13:15 +1000

    Add project files.

---
+++ Shannons ToDo List/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Shannons_ToDo_List
+{
+    partial class ToDoForm1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnHelloButton = new Button();
+            lblHelloLabel = new Label();
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            tabPage2 = new TabPage();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // btnHelloButton
+            // 
+            btnHelloButton.Location = new Point(75, 88);
+            btnHelloButton.Name = "btnHelloButton";
+            btnHelloButton.Size = new Size(132, 41);
+            btnHelloButton.TabIndex = 0;
+            btnHelloButton.Text = "Hello Button";
+            btnHelloButton.UseVisualStyleBackColor = true;
+            btnHelloButton.Click += btnHelloButton_Click;
+            // 
+            // lblHelloLabel
+            // 
+            lblHelloLabel.AutoSize = true;
+            lblHelloLabel.Location = new Point(93, 65);
+            lblHelloLabel.Name = "lblHelloLabel";
+            lblHelloLabel.Size = new Size(89, 20);
+            lblHelloLabel.TabIndex = 1;
+            lblHelloLabel.Text = "Hello World";
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Location = new Point(0, 1);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(800, 449);
+            tabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(btnHelloButton);
+            tabPage1.Controls.Add(lblHelloLabel);
+            tabPage1.Location = new Point(4, 29);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(792, 416);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "tabPage1";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Location = new Point(4, 29);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(792, 416);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "tabPage2";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // ToDoForm1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(tabControl1);
+            Name = "ToDoForm1";
+            Text = "Todo List";
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnHelloButton;
+        private Label lblHelloLabel;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+    }
+}
---
+++ Shannons ToDo List/Form1.cs
@@ -0,0 +1,16 @@
+namespace Shannons_ToDo_List
+{
+    public partial class ToDoForm1 : Form
+    {
+        public ToDoForm1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHelloButton_Click(object sender, EventArgs e)
+        {
+            lblHelloLabel.Text = "Hello User";
+        }
+
+    }
+}
