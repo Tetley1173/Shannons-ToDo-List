3d9f65ea19cb8b327a4b8ba852744b952c16884a
Shannon Tetley <tetley_73@hotmail.com>
2025-02-25 22:15:50 +1000

    Added a list that holds all of the ToDoList objects that are created. listCount can be deleted after testing.

--- Shannons ToDo List/Form1.cs
+++ Shannons ToDo List/Form1.cs
@@ -20,30 +20,33 @@ namespace Shannons_ToDo_List
 
 
         string[] tab1List;
+        //Refactor to use the toDoItems list once its implemented.
         int listCount = 0;
+        List<ToDoItem> toDoItems = new List<ToDoItem>();
         private void buttonAddListTab1_Click(object sender, EventArgs e)
         {
-
-            //This adds massive gaps and cuts half of the object out.
-            tabPage1.Controls.Add(new ToDoItem(listCount, "", flowLayoutPanelTab1));
+            toDoItems.Add(new ToDoItem(toDoItems.Count(), "", flowLayoutPanelTab1));
             listCount++;
         }
 
         private void buttonRemoveListTab1_Click(object sender, EventArgs e)
         {
             flowLayoutPanelTab1.Controls.Clear();
+            toDoItems.Clear();
             listCount = 0;
         }
     }
 
     //Class that creates objects containing buttons and text boxes needed to create each item in a list.
+    //It also adds itself to its parent control.
     //This is a Composite Control, info on how to make this is here:
     //https://learn.microsoft.com/en-us/dotnet/desktop/winforms/controls/windows-forms-control-development-basics?view=netframeworkdesktop-4.8
     //https://learn.microsoft.com/en-us/dotnet/desktop/winforms/controls/how-to-author-composite-controls?view=netframeworkdesktop-4.8
     class ToDoItem : UserControl
     {
         private int index;
-        private string text;
+        private int tab;
+        private string toDoText;
         private Button deleteBTN;
         private FlowLayoutPanel container;
         private RichTextBox textBox;
@@ -54,8 +57,12 @@ namespace Shannons_ToDo_List
             get;
             set;
         }
-
-        public string Text
+        public int Tab
+        {
+            get;
+            set;
+        }
+        public string ToDoText
         {
             get;
             set;
@@ -67,30 +74,24 @@ namespace Shannons_ToDo_List
             //Set text content
             if (inText == "")
             {
-                Text = "List item: ";
+                ToDoText = "List item: ";
             }
             else
             {
-                Text = inText;
+                ToDoText = inText;
             }            
             //Create UI elements
             //Style UI here
             FlowLayoutPanel container = new FlowLayoutPanel(); 
-            //This was breaking the displayed width of the object
-            //container.Dock = System.Windows.Forms.DockStyle.Fill;
-            //parent.Location = new System.Drawing.Point(0, 0);
             container.Name = "tab1Item" + index;
             container.Size = new System.Drawing.Size(parent.Width - 20, 132);
             container.BackColor = System.Drawing.Color.Blue;
-            //Spacing bug is not from a margin.
-            //container.Margin = new Padding(5,5,5,5);
             parent.Controls.Add(container);
 
-            textBox = new RichTextBox() { Text = Text + index, Height = 90, Width = 430, Parent = container };
+            textBox = new RichTextBox() { Text = ToDoText + index, Height = 90, Width = 430, Parent = container };
             container.Controls.Add(textBox);
             deleteBTN = new Button() { Parent = container, Text = "Delete", Height = 30, TextAlign = ContentAlignment.MiddleCenter};
             container.Controls.Add(deleteBTN);
-            //SetStyle(ControlStyles.ResizeRedraw, true);
         }
 
     }
